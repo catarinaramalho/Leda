42b9e3e1050022bf4297c49fdfb594d56602073e
121110708 CATARINA RAMALHO DOS SANTOS <catarina.santos@lcc3-084.lcc.ufcg.edu.br>
2022-11-01 09:33:54 -0300

    Ambientação do Roteiro 04

---
+++ R04-01-Rot-KLargestQuickSelectFloor-environment/src/main/java/orderStatistic/KLargestOrderStatisticsImpl.java
@@ -0,0 +1,52 @@
+package orderStatistic;
+
+/**
+ * Uma implementacao da interface KLargest que usa estatisticas de ordem para 
+ * retornar um array com os k maiores elementos de um conjunto de dados/array.
+ * 
+ * A k-esima estatistica de ordem de um conjunto de dados eh o k-esimo menor
+ * elemento desse conjunto. Por exemplo, considere o array [4,8,6,9,12,1]. 
+ * A 3a estatistica de ordem eh 6, a 6a estatistica de ordem eh 12.
+ * 
+ * Note que, para selecionar os k maiores elementos, pode-se pegar todas as 
+ * estatisticas de ordem maiores que k. 
+ * 
+ * Requisitos do algoritmo:
+ * - DEVE ser in-place. Porem, voce pode modificar o array original.
+ *   Voce pode criar ainda um array de tamanho k que vai armazenar apenas
+ *   os elementos a serem retornados.
+ * - Voce DEVE usar alguma ideia de algoritmo de ordenacao visto em sala 
+ *   para calcular estatisticas de ordem.
+ * - Se a entrada for invalida, deve-se retornar um array vazio (por exemplo,
+ *   ao solicitar os 5 maiores elementos em um array que soh contem 3 elementos).
+ *   Este metodo NUNCA deve retornar null.
+ * 
+ * @author campelo and adalberto
+ *
+ * @param <T>
+ */
+public class KLargestOrderStatisticsImpl<T extends Comparable<T>> implements KLargest<T>{
+
+	@Override
+	public T[] getKLargest(T[] array, int k) {
+		// TODO Implement your code here
+		throw new UnsupportedOperationException("Not implemented yet!");
+		//este metodo deve obrigatoriamente usar o orderStatistics abaixo.
+	}
+
+	/**
+	 * Metodo que retorna a k-esima estatistica de ordem de um array, usando
+	 * a ideia de algum algoritmo de ordenacao visto em sala. Caso nao exista 
+	 * a k-esima estatistica de ordem, entao retorna null.
+	 * 
+	 * Obs: o valor de k deve ser entre 1 e N.
+	 * 
+	 * @param array
+	 * @param k
+	 * @return
+	 */
+	public T orderStatistics(T[] array, int k){
+		// TODO Implement your code here
+		throw new UnsupportedOperationException("Not implemented yet!");		
+	}
+}
---
+++ R04-01-Rot-KLargestQuickSelectFloor-environment/src/main/java/problems/FloorBinarySearchImpl.java
@@ -0,0 +1,11 @@
+package problems;
+
+public class FloorBinarySearchImpl implements Floor {
+
+	@Override
+	public Integer floor(Integer[] array, Integer x) {
+		// TODO implement your code here
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+}
